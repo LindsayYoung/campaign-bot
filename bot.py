3cd252257cdae53e9c8c53c0bca19d8c8dcfc080
LindsayYoung <lindsay.n.young@gmail.com>
2016-08-21 23:31:56 -0400

    Updates for deploy

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ fec_params = {
     # don't want to flood the feed with repeats
     'min_receipt_date': datetime.now(),
 }
-
+api.update_status('deploy sucessful')
 while True:
     filings = requests.get('https://api.open.fec.gov/v1/efile/filings/?sort=-receipt_date&per_page=70', params=fec_params).json()
 
