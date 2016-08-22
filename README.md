ef5993aa63ca2e61246c966be46dffe6999cff07
LindsayYoung <lindsay.n.young@gmail.com>
2016-08-21 22:04:40 -0400

    Adding the running files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# campaign-bot
-A twitter bot that tweets out FEC filings
+# [@young_bots](https://twitter.com/young_bots)
+
+A twitter bot that consumes the [Federal Election Commission's API](https://api.open.fec.gov/developers/). 
+
+Right now it uses the real-time [efiling API feed](https://api.open.fec.gov/developers/#!/efiling/get_efile_filings) and tweets out the latest filings.
+
+This isn't official, it's just for fun.
---
+++ bot.py
@@ -0,0 +1,49 @@
+
+# !/usr/bin/env python
+#  -*- coding: utf-8 -*-
+import json
+import time
+from datetime import datetime
+
+import tweepy
+import requests
+
+from creds import *
+ 
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+# would want a db in the future if I do more complex things
+processed_files = []
+ 
+fec_params = {
+    'api_key': FEC_API_KEY,
+    # don't want to flood the feed with repeats
+    'min_receipt_date': datetime.now(),
+}
+
+while True:
+    filings = requests.get('https://api.open.fec.gov/v1/efile/filings/?sort=-receipt_date&per_page=70', params=fec_params).json()
+
+
+
+
+
+    if 'results' in filings:
+        for record in filings['results']:
+            if record['file_number'] not in processed_files:
+                committee_name = str(record['committee_name'] or '')[:116]
+                link = 'http://docquery.fec.gov/cgi-bin/forms/{0}/{1}'.format(record['committee_id'], record['file_number'])
+                message = committee_name + ' ' + link
+                if record['amends_file'] is not None:
+                    message = committee_name[:106] + ' ' + link +' amendment'
+                print (message)
+                processed_files.append(record['file_number'])
+
+                if len(processed_files) > 500:
+                    processed_files = processed_files[50:] 
+    
+    time.sleep(10)
+    
